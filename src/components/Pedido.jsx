3a54d9181245b673cf7bfd164f2df8ef01df5d09
Murillo Dourado <devmurillodourado@gmail.com>
2026-09-22 09:16:10 -0300

    feat: Métodos para realizar a entrega do pedido

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -1,9 +1,66 @@
+import { useState } from "react"
+
+// Array de Objetos contendo o estado inicial do cardápio
+const cardapio = [
+  { id: 1, nome: "Combo-01", preco: 25.00, disponivel: true, quantidade: 0 },
+  { id: 2, nome: "Combo-02", preco: 35.00, disponivel: false, quantidade: 0 },
+  { id: 3, nome: "Combo-03", preco: 45.00, disponivel: false, quantidade: 0 },
+  { id: 4, nome: "Combo-04", preco: 55.00, disponivel: true, quantidade: 0 },
+];
 
 const Pedido = () => {
+
+  //HOOK- useState- Manipula o estado da variavel
+  //Estados para gerenciar a lista de items do cardápio
+  const [items, setItems] = useState(cardapio);
+  const [status, setStatus] = useState("");
+  const [enviar, setEnviar] = useState(false);
+
+  //VALOR FIXO ADICIONADO AO TOTAL QUANDO TIVER NO CARRINHO
+  const taxaEntrega = 5.00;
+
+  //FUNÇÃO QUE ALTERA A QUANTIDADE DO PEDIDO
+  const AlterarQuantidade = (id, valor) => {
+    setItems(alt =>
+      //MAP: CRIAR UM NOVO E PERCORRE OS ITEMS SEM MODIFICAR O ORIGINAL(IMUTABILIDADE)
+      //TERNARIO: verifica se o item da iteração atual é que deve ser alterado
+      //SPREAD: (...item)- mantem os valor antigos e adiciona os novos
+      //MATH.max - Objeto que garante que a quantidade nunca será maior que 0
+      alt.map(item =>
+        item.id === id ? { ...item, quantidade: Math.max(0, item.quantidade + valor) } : item
+      )
+    )
+  }
+
+  //FILTER: seleciona apenas os produtos disponiveis no carrinho
+  const produtosDisponiveis = items.filter(item => item.disponivel);
+  const carrinho = items.filter(item => item.quantidade > 0);
+
+  //REDUCE: Calcula a soma dos items (preco + quantidade) 
+  // e adiciona a taxa de entrega
+
+  const subTotal = carrinho.reduce((ac, item) => ac + item.preco * item.quantidade, 0);
+  const total = subTotal > 0 ? subTotal + taxaEntrega : 0;
+
+  //SIMULAÇÃO DO CILCO DE VIDA DA ENTREGA USANDO TEMPORIZADOR ASSÍNCRONO
+
+  const ConfirmarPedido = () => {
+    setEnviar(true);
+    setStatus("Restaurante confirmou pagamento, preparando seu pedido!")
+    setTimeout(() => {
+      setStatus("Seu pedido saiu para entrega!")
+      setEnviar(false)
+    }, 5000) // 5 segundos
+    setTimeout(() => {
+      setStatus("Seu pedido foi entregue com sucesso!")
+      setEnviar(false)
+    }, 10000) // 10 segundos
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+
+    </>
   )
 }
 
